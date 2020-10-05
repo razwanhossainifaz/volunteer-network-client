9b482c1088ac5a4adf6e931b902f172513e6a60e
freelancerifaz <mrifaz369@gmail.com>
2020-10-06 00:06:16 +0600

    project done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -14,34 +14,34 @@ const Home = () => {
     }, [])
 
     return (
-        <>
-        <NavBar />
+        <main>
+            <NavBar />
 
-        <div className="topbar">
-            <h1 className="text-center mb-4">I grow by helping people in need.</h1>
-            <Form inline className="justify-content-center">
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-primary">Search</Button>
-            </Form> 
-        </div>
-
-        <div className="container">
-            <div className="row">
-                {
-                        organizations.map(work =>
-                        <div key={work.id} className="col-md-3 col-sm-6 col-xs-12 mb-3">
-                            <Link to={`/vregistration/${work.id}`}>
-                                <div className="img">
-                                    <img src={work.photo} alt="" />
-                                </div>
-                                <h2 className="title">{work.title}</h2>
-                            </Link>
-                        </div>
-                    )
-                }
+            <div className="topbar">
+                <h1 className="text-center mb-4 text-style">I grow by helping people in need.</h1>
+                <Form inline className="justify-content-center">
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2 search-style" />
+                    <Button variant="outline-primary">Search</Button>
+                </Form> 
             </div>
-        </div>
-        </>
+
+            <div className="container">
+                <div className="row">
+                    {
+                            organizations.map(work =>
+                            <div key={work.id} className="col-md-3 col-sm-6 col-xs-12 mb-3">
+                                <Link to={`/vregistration/${work.id}`}>
+                                    <div className="img">
+                                        <img src={work.photo} alt="" />
+                                    </div>
+                                    <h2 className="title">{work.title}</h2>
+                                </Link>
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
+        </main>
     );
 };
 
